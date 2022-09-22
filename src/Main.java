87d0bbd2231ca6f0f25c7e6b1e1da5f1c614f076
Harshpatel0815 <patelhj@mail.uc.edu>
2022-09-22 10:11:59 -0400

    Lab 2 for Module 4

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double costSummer = 160.71;
+        double costSpring = 257.95;
+        double costFall = 200.00;
+        double costWinter = 350.95;
+        double totalCost = costSummer + costSpring + costFall + costWinter;
+
+        System.out.println("Your maintenance cost for the summer is " + costSummer);
+        System.out.println("Your maintenance cost for the spring is " + costSpring);
+        System.out.println("Your maintenance cost for the fall is " + costFall);
+        System.out.println("Your maintenance cost for the winter is " + costWinter);
+        System.out.println("Your maintenance cost for the year is " + totalCost);
+
+
+
+    }
+}
